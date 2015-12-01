64d5866080db9e6767e867e5aba71c97537e26b5
adynata <clairewoods@gmail.com>
2015-11-30 21:12:27 -0800

    about page responsive

--- javascripts/moon.js
+++ javascripts/moon.js
@@ -13,40 +13,34 @@ $(function() {
   $(window).on('resize',function() {
     $('.moon-display').css('height', $('.moon-slide').css('width'));
     moonTextFit();
-    moonCurtainResize();
+  });
+
+  $(window).on('load',function() {
+    $('.moon-display').css('height', $('.moon-slide').css('width'));
+    moonTextFit();
   });
 
   function moonTextFit() {
     var height = $('.moon-article').css('height').slice(0,-2);
     $('.moon-article').textfill({
       maxFontPixels: 24,
-      minFontPixels: 10,
+      minFontPixels: 12,
       explicitHeight: height,
       debug: false
     });
   }
 
-  function moonCurtainResize() {
-    var height = $('#moon-1').css('height');
-    $('#moon-curtain').css('height', height.slice(0,-2));
-  }
-
-
-
   $('.thumbs img').on("click", function(e) {
     // take the border off of all thumbs
     $('.thumbs img').css('border', '2px black solid');
-    // $('.thumbs img').removeClass('active-moon');
     // put the border on the active thumb
     $(e.target).css('border', '2px white solid');
     $('.moon-slide').removeClass('active-moon');
     //display the selected thumb
     $('.moon-slide').css('z-index', '0');
-    // $('#moon-curtain').fadeIn();
     var parent = $(e.target).parent().attr('id');
     $('#' + parent.slice(0,-2)).addClass('active-moon');
     $('#' + parent.slice(0,-2)).css('z-index', '31');
-    // $('#moon-curtain').fadeOut();
   });
 
   function clickRight() {
